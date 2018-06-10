ab885e516b3f0e3575884ed30caffea1969ad24f
Voumo Tiogo Joel Brice <tiogoj@hotmail.com>
2018-06-10 19:47:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,9 @@
-const module = require('../array-test.js');
 var chocolateBars=["snickers", "hundred grand"," kitkat", "skittles"];
-var superheroines = ["catwoman", "she-hulk", "mystique"];
 
-superheroines.push("wonder woman");
+function addElementToBeginningOfArray(){
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(){
+  
+}
